522a0cd826d7257af9dee61d8e6b4ed2fd29ce66
=ssasshah1 <=ssasshah1@gmail.com>
2021-10-05 16:53:26 +0530

    Python backend added

--- scripts/upload_script.js
+++ scripts/upload_script.js
@@ -22,28 +22,49 @@ function fileSelect(){
 	const filename = fileInput.files[0];
 	console.log(filename)
 
-		checkmp3(filename)
+    checkmp3(filename)
 }
 
 function fileUpload(){
 
-	var uploaded_file = document.getElementById("file_select");
-	uploaded_file = uploaded_file.files[0]
-		uploaded_file_name = uploaded_file.name
-		uploaded_file._name = uploaded_file_name.replaceAll(" ", "_");
+    uploaded_file = document.getElementById("file_select");
+    uploaded_file = uploaded_file.files[0];
+    uploaded_file_name = uploaded_file.name;
+    uploaded_file._name = uploaded_file_name.replaceAll(" ", "_");
 
-	//	const FormData = require('form-data')
-	// import File System API
+    //	const FormData = require('form-data')
+    // import File System API
 
-	//	const fs = require('fs')
-	// create new FormData object
+    //	const fs = require('fs')
+    // create new FormData object
 
-	const formData = new FormData()
-		formData.append("audio_file", uploaded_file, uploaded_file._name);
-	formData.append("audio_name", uploaded_file_name);
+    formData = new FormData();
+    formData.append("audio_file", uploaded_file, uploaded_file._name);
+    formData.append("audio_name", uploaded_file_name);
+    formData.append("category", "uploaded");
 
+    console.log("NOW")
     for (var value of formData.values()) {
         console.log(value);
     } 
+}
 
+function runModel(){
+
+    file_name = formData.get("audio_name");
+    category = formData.get("category");
+    
+    var zmq = require(["zeromq"]);
+    sock = zmq.socket("push");
+
+    sock.bindSync("tcp://127.0.0.1:4242");
+
+    setInterval(function() {
+    sock.send(["model", file_name, category]);
+//        if(error || res !== 'model done') {
+//            console.error(error)
+//        } else {
+//            console.log("model done")
+//        }
+    }, 500)
 }
