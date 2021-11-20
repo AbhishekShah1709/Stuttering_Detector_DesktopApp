6301b320405affc092d5daba6685638cd4965bea
=ssasshah1 <=ssasshah1@gmail.com>
2021-11-20 12:58:08 +0530

    App completed

--- python_scripts/model.py
+++ python_scripts/model.py
@@ -4,26 +4,17 @@ from python_speech_features import mfcc
 from python_speech_features import delta
 import scipy.io.wavfile as wavy
 import numpy as np
-import tensorflow as tf
-import keras
-from keras.models import Model, load_model, Sequential
 import pickle
 from pydub import AudioSegment
-import sys
 from flask import Flask
 import json
-import cgi 
 from flask import request
-import base64
+from sklearn.ensemble import RandomForestClassifier
+
 app = Flask(__name__)
 
 @app.route("/upload", methods = ['POST'])
 def model():
-    f = open('./temp.txt', 'a')
-    f.write("start1")
-    f.close()
-
-    print("IN UPLOAD")
     body = request.data
 
     content = json.loads(body.decode('utf-8'))
@@ -35,20 +26,12 @@ def model():
     sound.export(wav_file_path, format="wav")
     newAudio = AudioSegment.from_wav(wav_file_path)
 
-    f = open('./temp.txt', 'a')
-    f.write("start2")
-    f.close()
-    
     (rate,sig) = wavy.read(wav_file_path)
     mfcc_feat = mfcc(sig,rate,numcep=15,nfilt=40,preemph=0.97)
     mfcc_d = delta(mfcc_feat,1)
     mfcc_dd = delta(mfcc_d,1)
     feat = np.concatenate((mfcc_feat,mfcc_d,mfcc_dd),axis=1)
     final_feats=[]
-    
-    f = open('./temp.txt', 'a')
-    f.write("start3")
-    f.close()
     
     for i in range(3,len(feat)-3):
         args = (feat[i-3],feat[i-2],feat[i-1],feat[i],feat[i+1],feat[i+2],feat[i+3])
@@ -58,18 +41,7 @@ def model():
     
     final_feats = np.array(final_feats)
 
-    f = open('./temp.txt', 'a')
-    f.write(os.getcwd())
-    f.close()
-    
-    print("BEFORE BTP SAV")
-#    sys.path.append("../../")
-    model = pickle.load(open('/home/abhishek/iiit/SEM 7/BTP-Flask/BTP_FP.sav', 'rb'))
-    print("AFTER BTP SAV")
-    
-    f = open('./temp.txt', 'a')
-    f.write("start5")
-    f.close()
+    model = pickle.load(open('./BTP_FP.sav', 'rb'))
     
     yhat_classes = model.predict(final_feats)
     a=0
@@ -99,11 +71,8 @@ def model():
     print(yhat_classes) ### yhat_classes is an array of 1 and 0, in which 1 represents detected filled pause at that frame and 0 represents normal speech
     ###For version of yhat_classes to time array, each frame is of .01 seconds. And total no. of frames equal to total no. of elements in yhat_classes
 
-    print(type(yhat_classes))
     json_yhat = yhat_classes.tolist()
-    print(type(json_yhat))
 
-    #dct = {'features': final_feats, 'output': yhat_classes}
     dct = {'output': json_yhat}
     json_obj = json.dumps(dct)
 
@@ -114,7 +83,6 @@ def model():
 
 @app.route("/record", methods = ['POST'])
 def model2():
-    print("IN RECORD")
     body = request.files['blob_details']
     
     file_name = str(uuid.uuid4())
@@ -141,19 +109,8 @@ def model2():
     
     final_feats = np.array(final_feats)
 
-    f = open('./temp.txt', 'a')
-    f.write("start6")
-    f.close()
+    model = pickle.load(open('./BTP_FP.sav', 'rb'))
     
-    print("BEFORE BTP SAV")
-    try:
-        model = pickle.load(open('./BTP_FP.sav', 'rb'))
-    except Exception as e:
-        f = open('./temp.txt', 'a')
-        f.write("\n"+ e)
-        f.close()
-    
-    print("AFTER BTP SAV")
     yhat_classes = model.predict(final_feats)
     a=0
     kt=0
@@ -182,11 +139,8 @@ def model2():
     print(yhat_classes) ### yhat_classes is an array of 1 and 0, in which 1 represents detected filled pause at that frame and 0 represents normal speech
     ###For version of yhat_classes to time array, each frame is of .01 seconds. And total no. of frames equal to total no. of elements in yhat_classes
 
-    print(type(yhat_classes))
     json_yhat = yhat_classes.tolist()
-    print(type(json_yhat))
     
-    #dct = {'features': final_feats, 'output': yhat_classes}
     dct = {'output': json_yhat}
     json_obj = json.dumps(dct)
     
@@ -195,5 +149,4 @@ def model2():
     
     return json_obj
 
-#if __name__ == "__main__":
 app.run(host='127.0.0.1', port=5000)
